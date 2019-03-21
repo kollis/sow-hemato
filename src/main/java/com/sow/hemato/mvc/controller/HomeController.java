34ae687a9fd54879d74b15549f2ea790919abb5e
kollis <kollis@users.noreply.github.com>
2019-03-21 09:34:31 -0400

    changed comment for test

--- src/main/java/com/sow/hemato/mvc/controller/HomeController.java
+++ src/main/java/com/sow/hemato/mvc/controller/HomeController.java
@@ -17,7 +17,7 @@ public class HomeController {
 	@RequestMapping("/home")
 	public ModelAndView home(ModelMap model) {
 		
-		System.out.println("\n*****in home of HomeController test*******");
+		System.out.println("\n*****in home of HomeController test1*******");
 		
 		
 		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
